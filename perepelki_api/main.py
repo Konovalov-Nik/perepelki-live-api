b97be53031d65d2e8febfd1205c3f4cb937a47f4
Nikita Konovalov <konovalovnikita@gmail.com>
2018-05-30 13:00:59 +0400

    Pre switch to uwsgi

--- perepelki_api/main.py
+++ perepelki_api/main.py
@@ -1,11 +1,12 @@
-from gevent.pywsgi import WSGIServer
+#from gevent.pywsgi import WSGIServer
 
 from perepelki_api.api.app import APP
 
 
 def main():
-    http_server = WSGIServer(('', 5000), APP)
-    http_server.serve_forever()
+    pass
+    #http_server = WSGIServer(('', 5000), APP)
+    #http_server.serve_forever()
 
 
 if __name__ == "__main__":
